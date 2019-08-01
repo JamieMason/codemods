1ad03c92a2c6acde99728f5dd3d4f984b440766c
Jamie Mason <jamie@foldleft.io>
2019-08-01 13:59:03 +0100

    feat(react): ignore spread elements when sorting props

--- transforms/sort-jsx-props.js
+++ transforms/sort-jsx-props.js
@@ -1,19 +1,47 @@
 export default (file, api) => {
   const j = api.jscodeshift;
+  const isEvenNumber = number => number % 2 === 0;
+  const isSpreadElement = prop => prop && prop.type === 'JSXSpreadAttribute';
+  const isValue = prop => prop && prop.type !== 'JSXSpreadAttribute';
   const getPropName = jsxAttribute => (jsxAttribute.name ? jsxAttribute.name.name : '...spread');
   const sortByPropName = (a, b) => {
     if (a < b) return -1;
     if (a > b) return 1;
     return 0;
   };
+  const sortProps = props => {
+    props.sort((a, b) => sortByPropName(getPropName(a), getPropName(b)));
+  };
 
   return j(file.source)
     .find(api.jscodeshift.JSXOpeningElement)
     .forEach(path => {
+      const chunks = [];
+      const nextAttributes = [];
       const jSXOpeningElement = path.value;
-      jSXOpeningElement.attributes = jSXOpeningElement.attributes.sort((a, b) =>
-        sortByPropName(getPropName(a), getPropName(b))
-      );
+
+      jSXOpeningElement.attributes.forEach((prop, i, props) => {
+        if (isValue(prop)) {
+          const prev = props[i - 1];
+          const next = props[i + 1];
+          const isChunkStart = !isValue(prev);
+          const isChunkEnd = !isValue(next);
+          const isWithinChunk = !isChunkStart && !isChunkEnd;
+
+          isChunkStart && chunks.push([]);
+          const [chunk] = chunks.slice(-1);
+          chunk.push(prop);
+
+          if (isChunkEnd) {
+            sortProps(chunk);
+            nextAttributes.push(...chunk);
+          }
+        } else if (isSpreadElement(prop)) {
+          nextAttributes.push(prop);
+        }
+      });
+
+      jSXOpeningElement.attributes = nextAttributes;
     })
     .toSource();
 };
