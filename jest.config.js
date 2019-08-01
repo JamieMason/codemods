93ca275b9180e63770e43d24c40199be1a3f7768
Jamie Mason <jamie@foldleft.io>
2019-08-01 12:33:54 +0100

    chore(jest): update deprecated config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverage: false,
-  setupTestFrameworkScriptFile: '<rootDir>/test/setup.js',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   testMatch: ['<rootDir>/test/codemods.spec.js']
 };
