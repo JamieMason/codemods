55f1599937a54d61d317ce9e89fbc19fbf667d1c
Jamie Mason <jamie@foldleft.io>
2019-08-01 12:29:54 +0100

    feat(objects): ignore spread elements when sorting members
    Closes #2

--- transforms/sort-object-props.js
+++ transforms/sort-object-props.js
@@ -1,19 +1,47 @@
 export default (file, api) => {
   const j = api.jscodeshift;
-  const getPropName = property => (property.key && (property.key.name || property.key.value)) || '';
+  const isEvenNumber = number => number % 2 === 0;
+  const isSpreadElement = prop => prop && prop.type === 'SpreadElement';
+  const isValue = prop => prop && prop.type !== 'SpreadElement';
+  const getPropName = prop => (prop.key && (prop.key.name || prop.key.value)) || '';
   const sortByPropName = (a, b) => {
     if (a < b) return -1;
     if (a > b) return 1;
     return 0;
   };
+  const sortProps = props => {
+    props.sort((a, b) => sortByPropName(getPropName(a), getPropName(b)));
+  };
 
   return j(file.source)
     .find(api.jscodeshift.ObjectExpression)
     .forEach(path => {
+      const chunks = [];
+      const nextProperties = [];
       const objectExpression = path.value;
-      objectExpression.properties = objectExpression.properties.sort((a, b) =>
-        sortByPropName(getPropName(a), getPropName(b))
-      );
+
+      objectExpression.properties.forEach((prop, i, props) => {
+        if (isValue(prop)) {
+          const prev = props[i - 1];
+          const next = props[i + 1];
+          const isChunkStart = !isValue(prev);
+          const isChunkEnd = !isValue(next);
+          const isWithinChunk = !isChunkStart && !isChunkEnd;
+
+          isChunkStart && chunks.push([]);
+          const [chunk] = chunks.slice(-1);
+          chunk.push(prop);
+
+          if (isChunkEnd) {
+            sortProps(chunk);
+            nextProperties.push(...chunk);
+          }
+        } else if (isSpreadElement(prop)) {
+          nextProperties.push(prop);
+        }
+      });
+
+      objectExpression.properties = nextProperties;
     })
     .toSource();
 };
