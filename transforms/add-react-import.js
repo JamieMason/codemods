c94ab27c09ab507093922891bcd0bdf8bf1b58ef
Jamie Mason <jamie.mason@shiftcommerce.com>
2020-10-13 12:27:03 +0100

    fix(react): handle when react is imported without default

--- transforms/add-react-import.js
+++ transforms/add-react-import.js
@@ -4,11 +4,16 @@ export default (file, api) => {
 
   const containsJsx = root.find(j.JSXIdentifier).length > 0;
 
-  const containsReactImport =
+  const containsReactPackageImport =
     root.find(j.ImportDeclaration).filter((importDeclaration) => importDeclaration.node.source.value === 'react')
       .length > 0;
 
-  if (containsJsx && !containsReactImport) {
+  const containsReactDefaultImport =
+    root
+      .find(j.ImportDefaultSpecifier)
+      .filter((importDefaultSpecifier) => importDefaultSpecifier.node.local.name === 'React').length > 0;
+
+  if (containsJsx && !(containsReactPackageImport && containsReactDefaultImport)) {
     root
       .get()
       .node.program.body.unshift(
