6b8b8609603c86f0fe00119b1e17472806d5272c
Jamie Mason <jamie@foldleft.io>
2024-12-05 16:41:09 +0000

    feat(react): move default props to JS default props

--- transforms/remove-react-default-props.js
+++ transforms/remove-react-default-props.js
@@ -1,14 +1,92 @@
 export default (file, api) => {
+  function removePath(path) {
+    return j(path).remove();
+  }
+
+  function isDefaultPropsAssignment(path) {
+    return path.node.left && path.node.left.property && path.node.left.property.name === 'defaultProps';
+  }
+
   const j = api.jscodeshift;
-  const removePath = (path) => j(path).remove();
-  const isAssigningDefaultProps = (e) =>
-    e.node.left && e.node.left.property && e.node.left.property.name === 'defaultProps';
+  const original = j(file.source);
+  const defaultPropsAssignment = original.find(j.AssignmentExpression).filter(isDefaultPropsAssignment);
 
-  const withoutAssignment = j(file.source)
-    .find(j.AssignmentExpression)
-    .filter(isAssigningDefaultProps)
-    .forEach(removePath)
-    .toSource();
+  if (defaultPropsAssignment.length === 0) {
+    return;
+  }
 
-  return withoutAssignment;
+  if (defaultPropsAssignment.length > 1) {
+    return console.log('SKIPPED: Multiple components in one file', file.path);
+  }
+
+  const defaultPropsProperties = defaultPropsAssignment.find(j.ObjectExpression).get('properties');
+
+  if (!defaultPropsProperties.value.length) {
+    return;
+  }
+
+  const componentName = defaultPropsAssignment.get(0).node.left.object.name;
+  // const componentAsFunctionDeclaration = original.find(j.FunctionDeclaration, { id: { name: componentName } });
+  const variableDeclarationWithComponentName = original.find(j.VariableDeclarator, { id: { name: componentName } });
+
+  // console.log({ keyValuePairs: defaultPropsProperties }, defaultPropsProperties.length);
+  // console.log({ componentName });
+  // console.log({ defaultedPropNames });
+
+  if (variableDeclarationWithComponentName.length > 0) {
+    const init = variableDeclarationWithComponentName.get(0).node.init;
+    if (!init.params) {
+      return console.log('SKIPPED: No params', file.path);
+    }
+    const propsArgument = init.params[0];
+    const propsAreDeconstructedObject = propsArgument.type === 'ObjectPattern';
+    if (propsAreDeconstructedObject) {
+      propsArgument.properties.forEach((prop) => {
+        if (!prop.key || prop.value.type === 'AssignmentPattern') return;
+        defaultPropsProperties.value.forEach((defaultProp) => {
+          if (defaultProp.key.name === prop.key.name) {
+            prop.value = j.assignmentPattern.from({
+              left: prop.value,
+              right: defaultProp.value,
+            });
+          }
+        });
+      });
+    }
+  }
+
+  // const { a, b } = this.props;
+  original
+    .find(j.VariableDeclarator, {
+      id: {
+        type: 'ObjectPattern',
+      },
+      init: {
+        type: 'MemberExpression',
+        object: {
+          type: 'ThisExpression',
+        },
+        property: {
+          name: 'props',
+        },
+      },
+    })
+    .forEach((path) => {
+      const properties = path.value.id.properties;
+      properties.forEach((prop) => {
+        if (!prop.key || prop.value.type === 'AssignmentPattern') return;
+        defaultPropsProperties.value.forEach((defaultProp) => {
+          if (defaultProp.key.name === prop.key.name) {
+            prop.value = j.assignmentPattern.from({
+              left: prop.value,
+              right: defaultProp.value,
+            });
+          }
+        });
+      });
+    });
+
+  defaultPropsAssignment.forEach(removePath);
+
+  return original.toSource();
 };
